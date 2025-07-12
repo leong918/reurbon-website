ce846eb1c3bec00fe79c6d610034bf687e5ed6be
leong918 <45736660+leong918@users.noreply.github.com>
2025-07-12 15:54:29 +0800

    Add Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {},
+  },
+  plugins: [],
+};
